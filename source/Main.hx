9b07379cfa3e9999a5b1c0b43da008b5911421ee
ZaTonnazzo <thatsdarkornot@gmail.com>
2026-07-01 18:06:11 +0200

    no splash screen

--- source/Main.hx
+++ source/Main.hx
@@ -10,7 +10,7 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(1280, 720, (FlxG.random.bool(5)) ? DepthsState : TitleState));
+		addChild(new FlxGame(1280, 720, (FlxG.random.bool(5)) ? DepthsState : TitleState, 60, 60, true));
 		FlxG.mouse.useSystemCursor = true;
 	}
 }
